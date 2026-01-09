0a22922855dd8f3a46f88fb16d2f4cfae57d8cd8
Nastya <nastyaivanova51679@mail.ru>
2026-01-09 14:45:48 +0300

    material_point

--- models/material_point.py
+++ models/material_point.py
@@ -1,1 +1,19 @@
-start
+import numpy as np
+
+
+class MaterialPoint:
+    """
+    Материальная точка в плоскости.
+    x: np.ndarray с координатами (x1, x2).
+    """
+
+    def __init__(self, x: np.ndarray):
+        self.x = x
+
+    def copy(self) -> "MaterialPoint":
+        """
+        self.x.copy() создаёт независимую копию массива (метод возвращает
+        новую материальную точку с такими же координатами),
+        чтобы изменение координат новой точки не влияло на исходную.
+        """
+        return MaterialPoint(self.x.copy())
